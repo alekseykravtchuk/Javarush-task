20a47255ca854612d2391ef2785442b428c58cfa
Aliaksei Krauchuk <alekseykravtchuk@gmail.com>
2019-03-06 22:18:46 +0300

    task1529 done

---
+++ task15/task1529/Plane.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly {
+    private int pasagers;
+    Plane (int pasagers) {
+        this.pasagers = pasagers;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
