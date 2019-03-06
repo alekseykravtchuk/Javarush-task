1a0e09f91b777ab35eb1eb1c11d18a310cc287be
Aliaksei Krauchuk <alekseykravtchuk@gmail.com>
2019-03-06 20:59:18 +0300

    1

---
+++ task12/task1216/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void vzic();
+        void bezdel();
+
+    }//add an interface here - добавь интерфейс тут
+
+}
