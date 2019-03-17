1e590bbc57f0db8fbf213933aaa1901af608718c
Aliaksei Krauchuk <alekseykravtchuk@gmail.com>
2019-03-17 18:08:46 +0300

    task1629 done

--- task16/task1629/Solution.java
+++ task16/task1629/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.task.task16.task1629;
 
-//import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
