22ce1f0252183729d7abd92bbfea9abb0bd7905b
Aliaksei Krauchuk <alekseykravtchuk@gmail.com>
2019-04-07 19:20:50 +0300

    task1812 done

---
+++ task18/task1812/QuestionFileOutputStream.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task18.task1812;
+
+import java.io.*;
+
+/* 
+Расширяем AmigoOutputStream
+*/
+
+public class QuestionFileOutputStream implements AmigoOutputStream {
+    AmigoOutputStream outputStream;
+    QuestionFileOutputStream (AmigoOutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
+    @Override
+    public void flush() throws IOException {
+        outputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        outputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        outputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        outputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        if(rd.readLine().equals("Д")) {
+            outputStream.close();
+        }
+    }
+}
+
